ba71906110bd6eba0433cedfd214deae6db7c810
Plamen Boudinov <boudinov@gmail.com>
2018-04-16 12:04:00 +0300

    format code

--- Code/SomeServiceHacked.cs
+++ Code/SomeServiceHacked.cs
@@ -11,17 +11,17 @@ using System.Threading.Tasks;
 
 namespace Code
 {
-    class NoiServiceHacked : SomeService
+    class SomeServiceHacked : SomeService
     {
         protected override WebRequest GetWebRequest(Uri uri)
         {
             var req = (HttpWebRequest)base.GetWebRequest(uri);
-			/*
-				This is the only viable hook i found, where we can install the internal delegate.
-				I tried using a thread, which monitors (by polling super frequently) the HttpWebRequest internals for the right moment to setup the internal delegate, but did not appear to work reliably.
-				In case you do not need to override the Server certificate validation, like it is done here, you need to provide a working Server certificate validation behavior (like https://stackoverflow.com/questions/28679120/how-to-call-default-servercertificatevalidationcallback-inside-customized-valida?lq=1)
-				Note, that at least, this is more fine grained (affects only this specific HttpWebRequest) than the common ServicePointManager.ServerCertValidationCallback, which affects all HttpWebRequests in the current process.
-			*/
+	    /*
+	    	This is the only viable hook i found, where we can install the internal delegate.
+	    	I tried using a thread, which monitors (by polling super frequently) the HttpWebRequest internals for the right moment to setup the internal delegate, but did not appear to work reliably.
+	    	In case you do not need to override the Server certificate validation, like it is done here, you need to provide a working Server certificate validation behavior (like https://stackoverflow.com/questions/28679120/how-to-call-default-servercertificatevalidationcallback-inside-customized-valida?lq=1)
+	    	Note, that at least, this is more fine grained (affects only this specific HttpWebRequest) than the common ServicePointManager.ServerCertValidationCallback, which affects all HttpWebRequests in the current process.
+	    */
             req.ServerCertificateValidationCallback = (object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) =>
             {
                 HttpWebRequestHack.SetupInternalClientCertDelegate(req);
