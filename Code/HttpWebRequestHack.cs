2906042582516e192f107bdcc0e682692750f59a
Plamen Boudinov <boudinov@gmail.com>
2018-04-16 11:59:23 +0300

    code and readme initial

---
+++ Code/HttpWebRequestHack.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Code
+{
+    public class HttpWebRequestHack
+    {
+        private static X509Certificate TheDelegate(string targetHost, X509CertificateCollection localCertificates, X509Certificate remoteCertificate, string[] acceptableIssuers)
+        {
+            return localCertificates[0];
+        }
+
+        public static void SetupInternalClientCertDelegate(HttpWebRequest request)
+        {
+            object submitWriteStream = request.GetType().GetField("_SubmitWriteStream", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(request);
+            object connection = submitWriteStream.GetType().GetField("m_Connection", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(submitWriteStream);
+
+            object tlsNetworkStream = connection.GetType().GetProperty("NetworkStream", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(connection);
+            //should be TlsStream, as at some point it gets replaced by NetworkStream = new TlsStream(...)
+            if (tlsNetworkStream != null && tlsNetworkStream.GetType().Name == "TlsStream")
+            {
+                object sslState = tlsNetworkStream.GetType().GetField("m_Worker", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(tlsNetworkStream);
+
+                //for some reason we cannot set _CertSelectionDelegate to anonymous Func<...>, neither can we Delegate.CreateDelegate from anonymous func
+                var internalDelegType = Assembly.GetAssembly(typeof(System.Net.Security.LocalCertificateSelectionCallback)).GetType("System.Net.Security.LocalCertSelectionCallback");
+                var deleg = Delegate.CreateDelegate(internalDelegType, typeof(HttpWebRequestHack).GetMethod(nameof(TheDelegate), BindingFlags.Static | BindingFlags.NonPublic));
+                sslState.GetType().GetField("_CertSelectionDelegate", BindingFlags.Instance | BindingFlags.NonPublic).SetValue(sslState, deleg);
+
+                //_Context type is SecureChannel
+                var context = sslState.GetType().GetField("_Context", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(sslState);
+                if (context != null)
+                    context.GetType().GetField("m_CertSelectionDelegate", BindingFlags.Instance | BindingFlags.NonPublic).SetValue(context, deleg);
+            }
+        }
+    }
+}
---
+++ Code/SomeServiceHacked.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Security;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Code
+{
+    class NoiServiceHacked : SomeService
+    {
+        protected override WebRequest GetWebRequest(Uri uri)
+        {
+            var req = (HttpWebRequest)base.GetWebRequest(uri);
+			/*
+				This is the only viable hook i found, where we can install the internal delegate.
+				I tried using a thread, which monitors (by polling super frequently) the HttpWebRequest internals for the right moment to setup the internal delegate, but did not appear to work reliably.
+				In case you do not need to override the Server certificate validation, like it is done here, you need to provide a working Server certificate validation behavior (like https://stackoverflow.com/questions/28679120/how-to-call-default-servercertificatevalidationcallback-inside-customized-valida?lq=1)
+				Note, that at least, this is more fine grained (affects only this specific HttpWebRequest) than the common ServicePointManager.ServerCertValidationCallback, which affects all HttpWebRequests in the current process.
+			*/
+            req.ServerCertificateValidationCallback = (object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) =>
+            {
+                HttpWebRequestHack.SetupInternalClientCertDelegate(req);
+                return true;
+            };
+
+            return req;
+        }
+
+    }
+}
